4864f4364098f3c7cdf1db5679147a4698a17d31
blxd <blxd@users.noreply.github.com>
2015-04-29 16:23:00 +0000

    support for the new tv catchup site

--- addon.py
+++ addon.py
@@ -1,29 +1,80 @@
+import re
+from xbmcswift2 import Plugin
 from operator import itemgetter
-import sys
-import xbmcgui
-import xbmcplugin
-import requests
+import urllib2
 
-if __name__ == "__main__":
-    addon_handle = int(sys.argv[1])
+CHANNEL_MAP = {1: "BBC One",
+               2: "BBC Two",
+               3: "ITV One",
+               4: "Channel 4",
+               5: "Channel 5",
+               17: "BBC News",
+               18: "CBBC",
+               24: "CBeeBies",
+               12: "BBC3",
+               13: "BBC4",
+               501: "MillenniumTV",
+               73: "Quest",
+               37: "VIVA",
+               31: "BBC Parliament",
+               78: "RT",
+               65: "BBC Red Button",
+               95: "Gala TV",
+               151: "Sail TV",
+               177: "Sub TV",
+               158: "Community Channel",
+               144: "S4C"}
 
-    xbmcplugin.setContent(addon_handle, 'livetv')
+plugin = Plugin()
 
-    cache_url = "http://tvcatchup.com/cache.php"
+M3U8_PATTERN = re.compile(r"\"(http://.*m3u8.*clientKey=[^\"]*)\";")
 
-    cache_data = requests.get(cache_url).json()
 
-    for channel_id, channel_data in sorted(cache_data['channels'].items(), key=itemgetter(0)):
+def http_get(url):
+    req = urllib2.Request(url)
+    try:
+        page = urllib2.urlopen(req)
+    except urllib2.HTTPError, e:
+        print e.fp.read()
+        raise
 
-        data_url = "http://tvcatchup.com/stream.php"
+    return page
 
-        stream_data = requests.get(data_url, params=dict(chan=channel_id)).json()
 
-        stream_url = stream_data.get('url')
+@plugin.route('/')
+def index():
+    return [{'label': name,
+             'path': plugin.url_for('play_stream', stream_id=cid),
+             'is_playable': True,
+             'thumbnail': 'http://tvcatchup.com/tvc-static//images/channels/v3/{0}.png'.format(cid)}
+            for cid, name in sorted(CHANNEL_MAP.items(), key=itemgetter(0))]
 
-        if stream_url:
-            img_url = "http://tvcatchup.com/tvc-static//images/channels/v3/{0}.png".format(channel_id)
-            li = xbmcgui.ListItem(str(channel_data['name']), iconImage=img_url)
-            xbmcplugin.addDirectoryItem(handle=addon_handle, url=stream_url, listitem=li)
 
-    xbmcplugin.endOfDirectory(addon_handle)
+@plugin.route('/stream/<stream_id>')
+def play_stream(stream_id):
+    stream_id = int(stream_id)
+    channel_name = CHANNEL_MAP[stream_id]
+
+    wurl = "http://tvcatchup.com/watch/{0}".format(stream_id)
+    plugin.log.debug("Getting watch url: {0}".format(wurl))
+    page = http_get(wurl)
+    content = page.read()
+
+    m3u8_match = M3U8_PATTERN.search(content, re.IGNORECASE | re.MULTILINE)
+
+    if m3u8_match:
+        stream_url = m3u8_match.group(1)
+        plugin.log.info("Loading streaming URL: {0}".format(stream_url))
+        plugin.set_resolved_url(stream_url)
+    elif "intended geographic area this channel" in content:
+        plugin.notify("The channel \"{0}\" is not available from this country".format(channel_name))
+        plugin.log.error("The channel \"{0}\" (stream_id={1}) is not "
+                         "available from this country".format(channel_name, stream_id))
+    else:
+        plugin.notify("The channel \"{0}\" is not currently available".format(channel_name))
+        plugin.log.error("An unknown error occurred")
+
+
+if __name__ == '__main__':
+    plugin.run()
+
